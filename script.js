550f3eb00822397cd1375bf4cdd2d52a4cbb09eb
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-15 12:54:43 -0600

    functinos working, kinda

--- script.js
+++ script.js
@@ -2,43 +2,52 @@ let display = [];
 let operadores = [];
 let values = [];
 let mainValues = [];
-let ans = 0; 
+let ans = 0;
 
-function sum(a, b) {
-  return a + b;
+function sum(value) {
+  return value.reduce((acum, values) => {
+    acum += values;
+    return acum;
+  }, 0);
 }
 
-function substract(a, b) {
-  return a - b;
+function substract(value) {
+  return value.reduce((acum, values) => {
+    acum -= values;
+    return acum;
+  }, 0);
+}
+function multiply(value) {
+  return value.reduce((acum, values) => {
+    acum *= values;
+    return acum;
+  }, 1);
 }
 
-function multiply(a, b) {
-  return a * b;
+function divide(value) {
+  return value.reduce((acum, values) => {
+    acum /= values;
+    return acum;
+  }, 1);
 }
 
-function divide(a, b) {
-  return a / b;
+function showResult() {
+  const ans = operate(operadores, mainValues);
+  document.querySelector(".result").innerHTML = ans;
+  operadores.pop();
 }
 
-function operate(operator, a, b) {
-  if (operator === "+") {
-    return sum(a, b);
-  } else if (operator === "-") {
-    return substract(a, b);
-  } else if (operator === "x") {
-    return multiply(a, b);
-  } else if (operator === "/") {
-    return divide(a, b);
+function operate(operator, values) {
+  if (operator[1] === "+") {
+    return sum(values);
+  } else if (operator[1] === "-") {
+    return substract(values);
+  } else if (operator[1] === "x") {
+    return multiply(values);
+  } else if (operator[1] === "/") {
+    return divide(values);
   }
 }
-
-
-function printAnswer(operador, a, b) {
-  document.querySelector("#equal").addEventListener("click", () => {
-    document.querySelector(".showOnScreen").innerHTML = operate(operador, a, b);
-  });
-}
-
 
 function clear() {
   document.querySelector(".showOnScreen").innerHTML = "";
@@ -46,7 +55,6 @@ function clear() {
     element.remove();
   });
   display.length = 0;
-  console.log(display)
 }
 
 function remove() {
@@ -56,26 +64,39 @@ function remove() {
   }
 }
 
+function clearScreen() {
+  document.querySelector(".showOnScreen").innerHTML = "";
+  display.forEach((element) => {
+    element.remove();
+  });
+}
+
 function main() {
   document.querySelectorAll(".item").forEach((element) => {
     element.addEventListener("click", () => {
       if (element.textContent === "AC") {
         clear();
+        console.log(mainValues);
       } else if (element.textContent === "C") {
         remove();
+        console.log(mainValues);
       } else {
-        if (element.textContent.match(/\+|\-|\x|\//)) {
+        if (element.textContent.match(/\+|\-|\x|\/|\=/)) {
           operadores.push(element.textContent);
           mainValues.push(parseInt(ans));
-          console.log(mainValues)
-        }else{
+          values.length = 0;
+          clearScreen();
+          console.log(mainValues);
+        } else {
           const newItem = document.createElement("div");
           newItem.textContent = element.textContent;
           document.querySelector(".showOnScreen").appendChild(newItem);
           display.push(newItem);
           values.push(element.textContent);
-          ans = values.join('');
-          console.log(ans)
+          ans = values.join("");
+        }
+        if (operadores.length === 2) {
+          showResult();
         }
       }
     });
